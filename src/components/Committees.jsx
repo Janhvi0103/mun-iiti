3489519e6cf9450e6a7c1393bc99dc823fada7cc
AKSHAT SHARMA <akshatgopal70@gmail.com>
2024-05-17 18:43:46 +0530

    final working model updated

--- src/components/Committees.jsx
+++ src/components/Committees.jsx
@@ -1,11 +1,15 @@
 import NavBar from "./NavBar";
 import headImg from "../assets/committees_header.jpg"
+import '../index.css';
+
 export default function Committees(){
     return (
         <>
             {/* <NavBar/> */}
-
-            <img src={headImg} alt="header" className=""/>
+            <div className="h-[85vh] w-[80vw]">
+                <img src={headImg} alt="header" width="100%" height="100%" />
+                <div className=" text-red-500 text-6xl">jjj</div>
+            </div>
         </>
     )
 }
