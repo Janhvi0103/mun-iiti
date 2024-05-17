8bc7f24418bfbb8bfbab2141ee7132e146a3b27b
AKSHAT SHARMA <akshatgopal70@gmail.com>
2024-05-17 18:10:54 +0530

    added basic committee page

---
+++ frontend/src/components/Committees.jsx
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar";
+import {headImg} from "../assets/committees_header.jpg"
+export default function Committees(){
+    return (
+        <>
+            {/* <NavBar/> */}
+
+        </>
+    )
+}
