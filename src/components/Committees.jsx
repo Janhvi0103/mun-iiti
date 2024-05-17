1a1b34b42d871313c00ecc4fa65460b1351be0d5
AKSHAT SHARMA <akshatgopal70@gmail.com>
2024-05-17 18:21:59 +0530

    fixed errors

--- src/components/Committees.jsx
+++ src/components/Committees.jsx
@@ -1,10 +1,11 @@
 import NavBar from "./NavBar";
-import {headImg} from "../assets/committees_header.jpg"
+import headImg from "../assets/committees_header.jpg"
 export default function Committees(){
     return (
         <>
             {/* <NavBar/> */}
 
+            <img src={headImg} alt="header" />
         </>
     )
 }
