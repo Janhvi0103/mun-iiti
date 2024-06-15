65c89bb71da6cf0d5c3d028087b0906b176107cc
AKSHAT SHARMA <akshatgopal70@gmail.com>
2024-06-15 15:53:56 +0530

    fixed error

--- src/components/Committees.jsx
+++ src/components/Committees.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Committees() {
   return (
-    <>
+    <div>
       
-    </>
+    </div>
   )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,13 @@
 import NavBar from "./NavBar";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CommitteesHead from "./Committees_Header";
+import CommitteesHeader from "./Committees_Header";
 
 function HomePage() {
   return (
     <>
       <NavBar />
-      <CommitteesHead />
-      <div className="App-header">
-        <h1>Welcome to MUN-IITI Homepage</h1>
-      </div>
+      <CommitteesHeader />
     </>
   );
 }
