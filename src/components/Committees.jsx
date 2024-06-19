ad17eefd637ace82e97a4ad8878ee9f8040422b3
AKSHAT SHARMA <akshatgopal70@gmail.com>
2024-06-19 23:46:17 +0530

    completed data and logo changes in committees

--- src/components/Committees.jsx
+++ src/components/Committees.jsx
@@ -4,46 +4,76 @@ import * as Dialog from "@radix-ui/react-dialog";
 
 export default function Committees() {
   const [open, setOpen] = useState(false);
+  const [delayedOpen, setdelayedOpen] = useState(true);
   const handleOpen = () => {
     setOpen(true);
-  }
+    setTimeout(() => {
+      setdelayedOpen(false);
+    }, 4000);
+    setdelayedOpen(true);
+  };
 
   return (
-    <div className="bg-gradient-to-b w-[100vw] from-slate-400 to-slate-300 overflow-x-hidden ">
-      <div className="flex justify-center items-center flex-wrap">
+    <div className="bg-gradient-to-b no-scrollbar from-slate-400 to-slate-300 overflow-x-hidden">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto">
         {data.map((ele, ind) => (
-          <Dialog.Root>
-            <div className="animate__animated animate__fadeInUp h-[45vh] w-[25vw] m-[6vh] cursor-pointer" key={ind}>
+          <Dialog.Root key={ind}>
+            <div
+              className={`animate__animated animate__fadeInUp w-[70%]  m-[6vh] cursor-pointer border-b-4 border-spacing-16 border-b-blue-700`}
+            >
               <Dialog.Trigger asChild>
                 <div
                   onClick={handleOpen}
-                  className="active:translate-x-[2vh] active:translate-y-[-2vh] active:bg-black active:bg-opacity-20 
-                  group
-                  rounded-xl transition-all flex-col items-center justify-center py-4 "
+                  className="active:translate-x-[2vh] active:translate-y-[-2vh] active:bg-black active:bg-opacity-20 group h-full rounded-xl transition-all flex flex-col items-center justify-between py-2"
                 >
+                  <div className="group-active:brightness-200 ">
                     <img
-                    height={"220px"}
-                    width={"220px"}
-                    src={ele.img}
-                    className={`active:invert mx-auto  ${ind == 1 ? "my-12" : "my-6"} `}
-                  />
-                  
-                  <div className="font-bold transition-all group-hover:shadow-xl text-[#1976D1] bg-blue-200 rounded-xl  md:text-xl lg:w-[35%] my-4 py-1 mx-auto">
+                      height={"250px"}
+                      width={"250px"}
+                      src={ele.img}
+                      className="mx-auto my-auto"
+                    />
+                  </div>
+
+                  <div className="font-semibold text-sm lg:text-lg transition-all  group-hover:shadow-xl group-hover:bg-opacity-90 text-[#1976D1] bg-blue-200 rounded-xl md:text-xl lg:px-5 px-3 bg-opacity-65 py-1 mt-8 mb-4 mx-auto">
                     {ele.name}
                   </div>
                 </div>
               </Dialog.Trigger>
+
               <Dialog.Portal>
                 <Dialog.Overlay className="" />
-                <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] transition-all rounded-[6px] bg-gradient-to-br from-blue-300 to-blue-100 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] animate__animated animate__zoomInRight">
-                  <Dialog.Title className=" m-0 text-md md:text-2xl underline underline-offset-8 font-semibold active:invert">
+                <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] transition-all  bg-gradient-to-br from-blue-300 to-blue-100 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] animate__animated rounded-xl animate__zoomInRight">
+                  <Dialog.Title className="m-0 text-md md:text-2xl underline decoration-blue-600 underline-offset-8 font-semibold">
                     {ele.name}
+                    <div
+                      className={`
+                        float-right
+                      ${
+                        ele.type === "offline"
+                          ? "bg-green-600"
+                          : "bg-orange-600"
+                      }
+                      capitalize
+                      inline-block
+                      bg-opacity-90
+                      text-sm
+                      tracking-wide
+                      mt-2
+                      text-gray-200 rounded xl px-2 pb-1 pt-0.5
+                      ${delayedOpen ? "animate-pulse" : "animate-none"} 
+                      `}
+                    >
+                      &#x25cf;
+                      &nbsp; {ele.type}
+                    </div>
                   </Dialog.Title>
-                  <Dialog.Description className=" mt-[20px] mb-5 text-[15px] text-base md:text-md font-semibold">
+                  <Dialog.Description className="mt-[20px] mb-5 text-[15px] text-base md:text-md font-semibold">
+                                      
                     {ele.desc}
                   </Dialog.Description>
                   <Dialog.Close asChild>
-                    <button className="bg-blue-500 text-white hover:bg-blue-600 outline-none px-[2vh] py-1 font-semibold rounded-2xl active:invert transition-all float-right">
+                    <button className="bg-red-600 bg-opacity-75 text-white hover:bg-red-600 outline-none px-[2vh] py-1 font-semibold rounded-2xl  transition-all float-right active:brightness-200">
                       Close &nbsp; &#10005;
                     </button>
                   </Dialog.Close>
@@ -52,6 +82,9 @@ export default function Committees() {
             </div>
           </Dialog.Root>
         ))}
+      </div>
+      <div className="h-[5vh] lg:h-[20vh]">
+{/* blank div */}
       </div>
     </div>
   );
--- src/components/committee_data.js
+++ src/components/committee_data.js
@@ -6,6 +6,7 @@ import c5 from "../assets/committees/5.png"
 import c6 from "../assets/committees/6.png"
 import c7 from "../assets/committees/7.png"
 import c8 from "../assets/committees/8.png"
+import c9 from "../assets/committees/c9.png"
 
 let data = [
   {
@@ -15,34 +16,28 @@ let data = [
     desc: "The UN General Assembly (UNGA) is the main policy-making organ of the Organization. Comprising all Member States, it provides a unique forum for multilateral discussion of the full spectrum of international issues covered by the Charter of the United Nations. Each of the Member States of the United Nations has an equal vote."
   },
   {
-    img: c2,
+    img: c8,
     name: "UNHRC",
     type: "offline",
     desc: "The Human Rights Council is an inter-governmental body within the United Nations system responsible for strengthening the promotion and protection of human rights around the globe and for addressing situations of human rights violations and make recommendations on them."
   },
   {
-    img: c3,
-    name: "UNSC",
+    img: c4,
+    name: "WHO",
     type: "offline",
-    desc: "The Security Council has primary responsibility for the maintenance of international peace and security. It takes the lead in determining the existence of a threat to the peace or act of aggression. The parties settle it by peaceful means and recommend methods of adjustment or terms of settlement. Under the Charter of the United Nations, all Member States are obligated to comply with Council decisions."
+    desc: "WHO committees simulate global health crises. Delegates representing nations strategize disease prevention, healthcare access, and pandemic response. They analyze data, develop healthcare policies, and negotiate resources to address health challenges, promoting international collaboration for disease control and healthcare equity."
   },
   {
-    img: c4,
+    img: c6,
     name: "AIPPM",
     type: "offline",
     desc: "All India Political Party Meet is a non-technical but powerful committee. It is a meeting between all the political parties of the nation. Typically called before the session of the Parliament or before the introduction of a bill, this committee aims to arrive at a consensus before the sessions begin."
   },
   {
-    img: c5,
+    img: c3,
     name: "UNSCW",
     type: "offline",
     desc: "UNCSW committees focus on gender equality and women's empowerment. Delegates representing nations, discuss policies, advocate for women's rights, and draft resolutions on issues such as education, violence, and workplace equity. They emphasize inclusive dialogue and collaborate to advance gender-related agendas."
-  },
-  {
-    img: c6,
-    name: "UNODC",
-    type: "online",
-    desc: "UNODC committees combat transnational crime and drug trafficking. Delegates, acting as member states, collaborate on policy strategies, legal frameworks, and international cooperation to counter narcotics, human trafficking, and organized crime. They engage in research, negotiation, and advocacy for effective global responses."
   },
   {
     img: c7,
@@ -51,10 +46,16 @@ let data = [
     desc: "UNODC committees combat transnational crime and drug trafficking. Delegates, acting as member states, collaborate on policy strategies, legal frameworks, and international cooperation to counter narcotics, human trafficking, and organized crime. They engage in research, negotiation, and advocacy for effective global responses."
   },
   {
-    img: c8,
-    name: "UNODC",
-    type: "online",
-    desc: "UNODC committees combat transnational crime and drug trafficking. Delegates, acting as member states, collaborate on policy strategies, legal frameworks, and international cooperation to counter narcotics, human trafficking, and organized crime. They engage in research, negotiation, and advocacy for effective global responses."
+    img: c9,
+    name: "IP",
+    type: "offline",
+    desc: "The International Press consists of a body of news agencies, broadcasters, and the like, who has their form of liberty in informing, and deliberating about events occurring in the United Nations."
+  },
+  {
+    img: c5,
+    name: "UNSC",
+    type: "offline",
+    desc: "The Security Council has primary responsibility for the maintenance of international peace and security. It takes the lead in determining the existence of a threat to the peace or act of aggression. The parties settle it by peaceful means and recommend methods of adjustment or terms of settlement. Under the Charter of the United Nations, all Member States are obligated to comply with Council decisions."
   }
 ]
 
