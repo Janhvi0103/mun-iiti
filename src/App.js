d63654c18ba291a12685d9669fd286f3a2c0f878
Gyanu Sahu <gyanuraj23330@gmai.com>
2024-11-16 15:48:27 +0530

    Resolved conflicts in Navbar.jsx and updated the Past Edition link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import HomePage from "./components/HomePage";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Sponser from './pages/Sponser';
-import Team from './pages/Team';
-import Committees from './components/Committees';
-=======
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./components/HomePage";
@@ -19,24 +6,17 @@ import Sponser from "./pages/Sponser";
 import Team from "./pages/Team";
 import Committees from "./components/Committees";
 import Past from "./App_past";
->>>>>>> 73d8f2e ( merged Past Edition Page)
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-<<<<<<< HEAD
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/sponsors' element={<Sponser/>}/>
-          <Route path='/team' element={<Team/>}/>
-          <Route path='/committees' element={<Committees/>}/>
-=======
           <Route path="/" element={<HomePage />} />
           <Route path="/sponsors" element={<Sponser />} />
           <Route path="/team" element={<Team />} />
           <Route path="/committees" element={<Committees />} />
           <Route path="/past-edition" element={<Past />} />
->>>>>>> 73d8f2e ( merged Past Edition Page)
         </Routes>
       </Router>
     </div>
